17c51d0921b29176b0d73f30eea714845bb0f137
Francisco Viana <149403389+franciscovmn@users.noreply.github.com>
2026-09-23 16:01:21 -0300

    Registra disciplina e dúvida no admin

--- duvidas/admin.py
+++ duvidas/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Disciplina, Duvida
+
+
+@admin.register(Disciplina)
+class DisciplinaAdmin(admin.ModelAdmin):
+    list_display = ["codigo", "nome", "ativa"]
+    list_filter = ["ativa"]
+    search_fields = ["codigo", "nome"]
+    filter_horizontal = ["monitores"]
+
+
+@admin.register(Duvida)
+class DuvidaAdmin(admin.ModelAdmin):
+    list_display = ["titulo", "disciplina", "autor", "monitor", "situacao", "criada_em"]
+    list_filter = ["situacao", "disciplina"]
+    search_fields = ["titulo", "descricao", "autor__username"]
+    readonly_fields = ["criada_em", "atualizada_em"]
