c0ef02118f7a07401450b4225f4adfc0cd403fec
Francisco Viana <149403389+franciscovmn@users.noreply.github.com>
2026-09-23 16:01:35 -0300

    Adiciona login, logout e cadastro de alunos

--- duvidas/views.py
+++ duvidas/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import Group
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+
+class CadastroView(CreateView):
+    form_class = UserCreationForm
+    template_name = "registration/cadastro.html"
+    success_url = reverse_lazy("login")
+
+    def form_valid(self, form):
+        resposta = super().form_valid(form)
+        alunos, _ = Group.objects.get_or_create(name="Alunos")
+        self.object.groups.add(alunos)
+        messages.success(self.request, "Conta criada. Entre com seu usuário e senha.")
+        return resposta
