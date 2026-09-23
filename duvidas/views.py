32e49250cdd9aea2b17932a3377ebcffbc6575cd
Francisco Viana <149403389+franciscovmn@users.noreply.github.com>
2026-09-23 16:02:08 -0300

    Mostra os horários de atendimento na tela de abrir dúvida

--- duvidas/views.py
+++ duvidas/views.py
@@ -6,7 +6,7 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 from .forms import DuvidaForm
-from .models import Duvida
+from .models import Disciplina, Duvida
 
 
 class CadastroView(CreateView):
@@ -26,6 +26,20 @@ class DuvidaCreateView(LoginRequiredMixin, CreateView):
     form_class = DuvidaForm
     template_name = "duvidas/nova.html"
     success_url = reverse_lazy("duvidas:nova")
+
+    def get_disciplina(self):
+        return Disciplina.objects.filter(
+            ativa=True, pk=self.request.GET.get("disciplina") or None
+        ).first()
+
+    def get_initial(self):
+        return {"disciplina": self.get_disciplina()}
+
+    def get_context_data(self, **kwargs):
+        contexto = super().get_context_data(**kwargs)
+        contexto["disciplinas"] = Disciplina.objects.filter(ativa=True)
+        contexto["disciplina"] = self.get_disciplina()
+        return contexto
 
     def form_valid(self, form):
         form.instance.autor = self.request.user
